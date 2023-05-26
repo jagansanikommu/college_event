8cc12452c0b11ac8501568ae27b89947a7f02a8e
Jaganmohan Reddy Sanikommu <69500541+jagansanikommu@users.noreply.github.com>
2023-05-26 13:07:30 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,request,redirect,url_for,send_from_directory
-from models import create_post
+from models import create_post,get_posts
 
 app = Flask(__name__)
 
